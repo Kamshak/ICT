9ad523e8f128aabf103f8e9c6e005e4327d9eefd
Valentin Funk <funk.valentin@gmail.com>
2015-12-18 23:50:13 +0000

    Progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,13 @@
 console.log('\'Allo \'Allo!');
 
 $(document).ready(function() {
+  // Initialize Stellar Library (parallax backgrounds)
    $.stellar({
       horizontalScrolling: false,
+      responsive: true
    });
 
+   // Show menu once scrolled past
   var waypoint = new Waypoint({
     element: document.getElementById('navigation'),
     handler: function(direction) {
@@ -12,5 +15,36 @@ $(document).ready(function() {
     }
   })
 
-   console.log("DOne");
+  // Responsive parallax backgrounds
+  var fakeListenerParallax;
+  fakeListenerParallax = setInterval((function() {
+    $('[data-stellar-background-ratio]').each(function() {
+      var ofs;
+      ofs = $(this).offset().top * $(this).data("stellar-background-ratio");
+      ofs = -ofs;
+      ofs = ofs + $(window).scrollTop() * 0.5;
+      $(this).css("background-position", "0px " + ofs + "px");
+    });
+  }), 15);
+
+  setTimeout((function() {
+    clearInterval(fakeListenerParallax);
+  }), 200);
+
+  // Initialize smooth scroll
+  smoothScroll.init({
+    selector: '[href]', // Selector for links (must be a valid CSS selector)
+    selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
+    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInOutCubic', // Easing pattern to use
+    updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+    offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+  });
+
+  // Close the menu after clicking
+  $(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+  });
 });
